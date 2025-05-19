56b55783b6d67d168691cdb8f44d2f61c1abb688
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-05-19 12:09:36 +0300

    user controller

---
+++ src/controllers/user/index.ts
@@ -0,0 +1,72 @@
+import { Request, Response } from "express";
+import ctrlWrapper from "../../helpers/ctrlWrapper";
+import { extendedUser } from "../../interfacesTypes";
+import User from "../../models/user.model";
+import bcryptjs from "bcryptjs";
+
+import errorHandler from "../../helpers/errorHandler";
+import { IUpdateUser } from "./interface";
+
+const getInfo = async (req: Request, res: Response) => {
+  const { email, name, allTasks } = (req as extendedUser).user;
+
+  res.status(200).json({ email, name, allTasks });
+};
+
+const updateUser = async (req: Request, res: Response) => {
+  const { _id, email, name, password } = (req as extendedUser).user;
+
+  const { email: newEmail, name: newName, password: newPassword } = req.body;
+
+  const updates: Partial<IUpdateUser> = {};
+
+  if (newEmail && email !== newEmail) {
+    updates.email = newEmail;
+  }
+
+  if (newName && name !== newName) {
+    updates.name = newName;
+  }
+
+  if (newPassword) {
+    const comparePasswords = await bcryptjs.compare(newPassword, password);
+
+    if (!comparePasswords) {
+      updates.password = await bcryptjs.hash(newPassword, 10);
+    }
+  }
+
+  if (Object.keys(updates).length === 0) {
+    return errorHandler(
+      400,
+      "No changes detected. All fields match the current data."
+    );
+  }
+
+  const newUser = await User.findByIdAndUpdate(_id, updates, { new: true });
+
+  return res.status(200).json(newUser);
+};
+
+const deleteAccount = async (req: Request, res: Response) => {
+  const { _id } = (req as extendedUser).user;
+
+  await User.findByIdAndUpdate(_id);
+
+  return res
+    .clearCookie("token", {
+      maxAge: 3 * 24 * 60 * 60 * 1000,
+      // sameSite: "none",
+      sameSite: "lax",
+      secure: true,
+      path: "/",
+    })
+    .status(204)
+    .json();
+};
+
+export default {
+  getInfo: ctrlWrapper(getInfo),
+  updateUser: ctrlWrapper(updateUser),
+  deleteAccount: ctrlWrapper(deleteAccount),
+};
