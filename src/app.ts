d32f0fc9488bb84e14fd54c0f412b3647865259b
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-05-19 12:07:18 +0300

    add some packages

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express, { Request, Response } from "express";
 import router from "./routes";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
-app.use(express.json());
 
+app.use(cors());
+app.use(express.json());
 app.use(cookieParser());
 
 app.use(router);
