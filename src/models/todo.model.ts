2f61034062e7d410dd5c88701916e3c392b9104f
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-05-20 12:36:31 +0300

    todo schema

--- src/models/todo.model.ts
+++ src/models/todo.model.ts
@@ -2,6 +2,10 @@ import { model, Schema, Types } from "mongoose";
 import { IToDo } from "./types/todo.schema";
 import helpers from "../helpers";
 
+const statuses = ["todo", "progress", "done"];
+
+const date = new Date(Date.now() + 24 * 60 * 60 * 1000);
+
 const toDoSchema = new Schema<IToDo>(
   {
     title: { type: String, required: true },
@@ -9,8 +13,14 @@ const toDoSchema = new Schema<IToDo>(
     owner: { type: Schema.Types.ObjectId, ref: "user", required: true },
     endTime: {
       type: String,
-      required: true,
-      default: new Date().toLocaleDateString(),
+      // required: true,
+      default: date,
+    },
+    status: {
+      type: String,
+      enum: statuses,
+      default: statuses[0],
+      // required: true,
     },
   },
   { timestamps: true }
--- src/models/types/todo.schema.ts
+++ src/models/types/todo.schema.ts
@@ -3,7 +3,8 @@ import { Types } from "mongoose";
 export interface IToDo {
   title: string;
   description?: string;
-  endTime: string | Date;
+  endTime?: string | Date;
   owner: Types.ObjectId;
+  status?: string;
   [x: string]: any;
 }
